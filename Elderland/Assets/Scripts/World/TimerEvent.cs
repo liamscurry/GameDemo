99267a6f8f018b6226776cff48d3b8a053fbab01
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-07-04 13:54:28 -0500

    Added/updated tutorial for combat elements to reflect improvements.

---
+++ Elderland/Assets/Scripts/World/TimerEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerEvent : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent timerEvent;
+    [SerializeField]
+    private float delay;
+    [SerializeField]
+    private bool executed;
+
+    public void Enable()
+    {
+        executed = false;
+        StopAllCoroutines();
+    }
+
+    public void Disable()
+    {
+        executed = true;
+        StopAllCoroutines();
+    }
+
+    public void Trigger()
+    {
+        if (!executed)
+        {
+            executed = true;
+            StartCoroutine(TimerCoroutine());
+        }
+    }
+
+    private IEnumerator TimerCoroutine()
+    {
+        yield return new WaitForSeconds(delay);
+        if (timerEvent != null)
+            timerEvent.Invoke();
+    }
+}
