23971ec26f8eeeda056063e943359061f45fa140
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-07-21 15:37:37 -0500

    Added placement functionality for vineplacer editor.

---
+++ Elderland/Assets/Scripts/Creation/VinePlacer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityEditor
+{
+    [ExecuteInEditMode]
+    public class VinePlacer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        /*
+        private void OnGUI()
+        {
+            Event currentEvent = Event.current;
+            GUI.Box(new Rect(0,0, Screen.width, Screen.height), "Whole Screen");
+            Debug.Log("wow");
+            if (currentEvent.button == 0)
+            {
+                Debug.Log("clicked");
+            }
+        }*/
+    }
+}
---
+++ Elderland/Assets/Scripts/Creation/VinePlacerEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityEditor
+{
+    [ExecuteInEditMode]
+    [CustomEditor(typeof(VinePlacer))]
+    public class VinePlacerEditor : Editor
+    {
+        void OnSceneGUI()
+        {
+            // Learned in "Disable mouse selection in editor view" unity answer.
+            int eventID = GUIUtility.GetControlID(FocusType.Passive);
+
+            if (Event.current.type == EventType.MouseDown)
+            {
+                if (Event.current.button == 0 && Event.current.isMouse)
+                {
+                    // Learned in "Disable mouse selection in editor view" unity answer.
+                    GUIUtility.hotControl = eventID;
+                    Event.current.Use();
+
+                    Ray mousePositionRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(mousePositionRay, out hitInfo, 100f, LayerConstants.GroundCollision))
+                    {
+                        //Debug.DrawLine(hitInfo.point, hitInfo.point + hitInfo.normal, Color.magenta, 5f);
+                        Debug.Log(((VinePlacer) target));
+                        Quaternion normalRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                        GameObject healthPickupPrefab = 
+                            Resources.Load<GameObject>(ResourceConstants.Pickups.HealthPickup);
+                        GameObject instanced = 
+                            GameObject.Instantiate(healthPickupPrefab, hitInfo.point, normalRotation);
+                        instanced.transform.parent = ((VinePlacer) target).gameObject.transform;
+                        Debug.Log(hitInfo.transform.name);
+                        //Debug.Log(hitInfo.point);
+                    }
+                }
+            }
+        }
+    }
+}
