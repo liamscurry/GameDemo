8197dc9502954b4e58edd4cb8ced179d4f67cb52
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-08-29 16:55:11 -0500

    Added Billboard LOD for standard oak trees.

---
+++ Elderland/Assets/Scripts/Constructs/HorizontalBillboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+public class HorizontalBillboard : MonoBehaviour
+{
+    void Update()
+    {
+        if (Camera.current != null)
+        {
+            Vector3 direction = 
+                (Camera.current.transform.position - transform.position).normalized;
+
+            transform.rotation = Quaternion.LookRotation(Vector3.up, direction);
+        }
+
+        //Debug.Log(direction);
+        //Debug.Log("executing");
+    }
+}
