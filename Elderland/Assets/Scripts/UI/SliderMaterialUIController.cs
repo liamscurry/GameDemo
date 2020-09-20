e7f11f9059a3ce21a0b4b6a59fa1cecb289b8372
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-09-19 20:38:33 -0500

    Fixed cutscene gameplay UI #43
    - Problem: during cutscene some gameplay UI used to be on when it shouldn't be.
    - Solution: reparented new UI elements to existing elements to be properly toggled.
    - Fixed miscellanious bugs with custom mask assignment and asset clipping.

--- Elderland/Assets/Scripts/UI/SliderMaterialUIController.cs
+++ Elderland/Assets/Scripts/UI/SliderMaterialUIController.cs
@@ -6,17 +6,27 @@ using UnityEngine.UI;
 public class SliderMaterialUIController : MonoBehaviour
 {
     private Image image;
+    private bool initialized;
 
     private void Awake()
     {
-        image = GetComponent<Image>();
-        image.material = new Material(image.material); // Allows individual instances of sliders to have their own value.
-        image.material.SetFloat("_HorizontalOnPercentage", 1);
+        TryInitialize();
+    }
+
+    private void TryInitialize()
+    {
+        if (!initialized)
+        {
+            image = GetComponent<Image>();
+            image.material = new Material(image.material); // Allows individual instances of sliders to have their own value.
+            image.material.SetFloat("_HorizontalOnPercentage", 1);
+            initialized = true;
+        }
     }
 
     public void UpdateMaterialSliderFloat(Slider slider)
     {
+        TryInitialize();
         image.material.SetFloat("_HorizontalOnPercentage", slider.value);
-        Debug.Log(slider.value);
     }
 }
