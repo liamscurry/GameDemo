be34fc24a6f50a1eb890778706a83d45ad3024a5
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-09-26 15:02:33 -0500

    Fixed geometry colliders on reward chambers to prevent player getting stuck on ledge.

--- Elderland/Assets/Scripts/Game/MenuManager.cs
+++ Elderland/Assets/Scripts/Game/MenuManager.cs
@@ -29,7 +29,7 @@ public class MenuManager : MonoBehaviour
     {
         if (!pausedLastFrame)
         {
-            PauseMenu();
+            //PauseMenu();
             StartMenu();
             ObjectiveMenu();
         }
