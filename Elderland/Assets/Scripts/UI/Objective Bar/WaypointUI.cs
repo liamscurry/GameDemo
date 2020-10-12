ea285face960219992209198098387089ae4c781
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-10-12 18:00:54 -0500

    Fixed issue where waypoint UI would stutter when looking almost directly at it.

--- Elderland/Assets/Scripts/UI/Objective Bar/WaypointUI.cs
+++ Elderland/Assets/Scripts/UI/Objective Bar/WaypointUI.cs
@@ -8,12 +8,16 @@ public class WaypointUI : MonoBehaviour
 {
     [SerializeField]
     private Vector3 worldPosition;
+    [SerializeField]
+    private Image directionIndicator;
 
     private Text distanceText;
+    private CanvasScaler canvasScaler;
 
     private void Awake()
     {
         distanceText = GetComponentInChildren<Text>();
+        canvasScaler = GetComponentInParent<CanvasScaler>();
     }
 
     private void Update()
@@ -21,127 +25,58 @@ public class WaypointUI : MonoBehaviour
         if (Camera.current != null)
         {
             Vector3 waypointScreenPosition =
-                Camera.current.WorldToScreenPoint(worldPosition);
+                Camera.current.WorldToViewportPoint(worldPosition);
+            Vector2 screenSize = canvasScaler.referenceResolution;
+            waypointScreenPosition.x *= screenSize.x;
+            waypointScreenPosition.y *= screenSize.y;
+            //Debug.Log(waypointScreenPosition.x);
             Vector3 waypointCameraPosition = Camera.current.worldToCameraMatrix.MultiplyPoint(worldPosition);
-
-            float waypointPolarAngle =
-                Matho.Angle(new Vector2(waypointCameraPosition.x, -waypointCameraPosition.z));
-            Vector2 waypointClampOffset =
-                new Vector2(Mathf.Cos(waypointPolarAngle * Mathf.Deg2Rad), Mathf.Sin(waypointPolarAngle * Mathf.Deg2Rad));
-
-            /*
-            Vector3 waypointClampPosition = 
-                new Vector3(Camera.current.pixelWidth / 2, Camera.current.pixelHeight / 2, 0) + 
-                new Vector3(waypointClampOffset.x * Camera.current.pixelHeight * 4f, waypointClampOffset.y * Camera.current.pixelHeight * 4f, 0);
-            */
 
             Vector3 waypointCameraDirection = waypointCameraPosition.normalized;
             Vector2 projectedWaypointCameraDirection = new Vector2(waypointCameraDirection.x, waypointCameraDirection.y).normalized;
+
+            float radius = canvasScaler.referenceResolution.y * .45f;
+            //float radius = Camera.current.pixelHeight * .45f;
+            //Vector2 screenSize = new Vector2(Camera.current.pixelWidth, Camera.current.pixelHeight);
+
             Vector3 waypointClampDirection = 
-                new Vector3(Camera.current.pixelWidth / 2, Camera.current.pixelHeight / 2, 0) + 
-                new Vector3(projectedWaypointCameraDirection.x * Camera.current.pixelHeight * .4f, projectedWaypointCameraDirection.y * Camera.current.pixelHeight * .4f, 0);
-
-            float radius = Camera.current.pixelHeight * .4f;
-
-            //Debug.Log(waypointCameraPosition.z);
+                new Vector3(screenSize.x / 2, screenSize.y / 2, 0) + 
+                new Vector3(projectedWaypointCameraDirection.x * radius, projectedWaypointCameraDirection.y * radius, 0);
 
             if ((new Vector2(waypointScreenPosition.x, waypointScreenPosition.y) -
-                new Vector2(Camera.current.pixelWidth / 2f, Camera.current.pixelHeight / 2f)).magnitude > radius ||
+                new Vector2(screenSize.x / 2f, screenSize.y / 2f)).magnitude > radius ||
                 waypointCameraPosition.z > 0)
             {
                 waypointScreenPosition = waypointClampDirection;
+
+                if (!directionIndicator.IsActive())
+                    directionIndicator.gameObject.SetActive(true);
+
+                UpdateDirectionIndicator(projectedWaypointCameraDirection, 25f);
+            }
+            else
+            {
+                if (directionIndicator.IsActive())
+                    directionIndicator.gameObject.SetActive(false);
             }
 
-
-
-            /*
-            waypointClampDirection.x =
-                Mathf.Clamp(waypointClampDirection.x, Camera.current.pixelWidth * 0.1f, Camera.current.pixelWidth * 0.9f);
-
-            waypointClampDirection.y =
-                Mathf.Clamp(waypointClampDirection.y, Camera.current.pixelHeight * 0.1f, Camera.current.pixelHeight * 0.9f);*/
-
-            /*
-            if (waypointClampPosition.x > Camera.current.pixelWidth * 0.9f)
-            {
-                waypointClampPosition.x = Camera.current.pixelWidth * 0.9f;
-            }
-            else if (waypointClampPosition.x < Camera.current.pixelWidth * 0.1f)
-            {
-                waypointClampPosition.x = Camera.current.pixelWidth * 0.1f;
-            }
-
-            if (waypointClampPosition.y > Camera.current.pixelHeight * 0.9f)
-            {
-                waypointClampPosition.y = Camera.current.pixelHeight * 0.9f;
-            }
-            else if (waypointClampPosition.y < Camera.current.pixelHeight * 0.1f)
-            {
-                waypointClampPosition.y = Camera.current.pixelHeight * 0.1f;
-            }*/
-
-
-            //Vector2 centerScreenPositionOffset =
-            //    new Vector2(waypointScreenPosition.x, waypointScreenPosition.y) - new Vector2(Camera.current.pixelWidth / 2, Camera.current.pixelHeight / 2);
-            
-
-            
-            /*
-            if (waypointScreenPosition.x < Camera.current.pixelWidth * 0.1f ||
-                waypointScreenPosition.x > Camera.current.pixelWidth * 0.9f)
-            {   
-                waypointScreenPosition.x = waypointClampDirection.x;
-            }
-
-            if (waypointScreenPosition.y < Camera.current.pixelHeight * 0.1f ||
-                waypointScreenPosition.y > Camera.current.pixelHeight * 0.9f)
-            {   
-                waypointScreenPosition.y = waypointClampDirection.y;
-            }*/
-                // * (1 - Matho.AngleBetween(new Vector3(0, 0, 1), waypointCameraPosition) / 180f)
-            /*
-            waypointScreenPosition = 
-                new Vector3(Camera.current.pixelWidth / 2, Camera.current.pixelHeight / 2, 0) +
-                (new Vector3(200 * Mathf.Sign(waypointCameraPosition.x), -200 * Mathf.Sign(waypointCameraPosition.z), 0));*/
-
-                // * (1 - Matho.AngleBetween(new Vector3(0, 0, 1), waypointCameraPosition) / 180f)
-            
-            /*waypointScreenPosition =
-                new Vector3(Camera.current.pixelWidth / 2, Camera.current.pixelHeight / 2, 0) +
-                new Vector3(200 * Mathf.Sign(waypointCameraPosition.x), 0, 0) +
-                new Vector3(0, -200 * Mathf.Sign(waypointCameraPosition.z), 0);*/
-            //Debug.Log(Mathf.Cos(200 * waypointPolarAngle * Mathf.Deg2Rad));
-            /*
-            //waypointScreenPosition.x = waypointScreenPosition.x % Camera.current.pixelWidth;
-            Debug.Log(waypointScreenPosition.x + ", " + waypointScreenPosition.y);
-            if (waypointScreenPosition.x < Camera.current.pixelWidth * 0.1f ||
-                waypointScreenPosition.x > Camera.current.pixelWidth * 0.9f)
-            {
-                Vector3 waypointCameraDisplacement = worldPosition - Camera.current.transform.position;
-                float horizontalAngle = Matho.AngleBetween(waypointCameraDisplacement, Camera.current.transform.forward);
-                waypointScreenPosition.x = Camera.current.pixelWidth * 0.1f;
-            }
-            */
-            /*
-            if (Mathf.Abs(waypointScreenPosition.x) < Camera.current.pixelWidth * 0.1f ||
-                Mathf.Abs(waypointScreenPosition.x) > Camera.current.pixelWidth * 0.9f)
-            {
-                waypointScreenPosition.x = Camera.current.pixelWidth / 2f + (Camera.current.pixelWidth / 2f) * 0.95f * waypointClampPosition.x;
-            }
-
-            if (Mathf.Abs(waypointScreenPosition.y) < Camera.current.pixelHeight * 0.1f ||
-                Mathf.Abs(waypointScreenPosition.y) > Camera.current.pixelHeight * 0.9f)
-            {
-                waypointScreenPosition.y = Camera.current.pixelHeight / 2f + (Camera.current.pixelHeight / 2f) * 0.95f * waypointClampPosition.y;
-            }*/
-
-            /*
-            if (Mathf.Abs(waypointScreenPosition.y) < Camera.current.pixelHeight * 0.1f)
-                waypointScreenPosition.y = Camera.current.pixelHeight * 0.1f;
-            if (Mathf.Abs(waypointScreenPosition.y) > Camera.current.pixelHeight * 0.9f)
-                waypointScreenPosition.y = Camera.current.pixelHeight * 0.9f;*/
             ((RectTransform) transform).anchoredPosition = waypointScreenPosition;
+
+            UpdateDistanceText();
         }
+    }
+
+    private void UpdateDistanceText()
+    {
+        float distance =
+            (PlayerInfo.Player.transform.position - worldPosition).magnitude;
+
+        distanceText.text = Mathf.RoundToInt(distance) + "m";
+    }
+
+    private void UpdateDirectionIndicator(Vector2 direction, float radius)
+    {
+        ((RectTransform) directionIndicator.transform).anchoredPosition = direction * radius;
     }
 
     private void OnDrawGizmosSelected()
