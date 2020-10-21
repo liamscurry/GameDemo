f48add4679c27cecd2763487c371932d00630f1c
liamscurry <32856600+liamscurry@users.noreply.github.com>
2020-10-21 11:17:02 -0500

    Fixed bug with light enemy intro where UI was freezing input. Implemented custom script to popups.

---
+++ Elderland/Assets/Scripts/World/PopupEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PopupEvent : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent acceptEvent;
+    //[SerializeField]
+    //private bool executed;
+
+    /*
+    public void Enable()
+    {
+        executed = false;
+    }
+
+    public void Disable()
+    {
+        executed = true;
+    }
+    */
+    
+    public void Update()
+    {
+        if (Input.GetKeyDown(GameInfo.Settings.UseKey))
+        {
+            //if (!executed)
+            {
+                //executed = true;
+                if (acceptEvent != null)
+                    acceptEvent.Invoke();
+            }
+        }
+    }
+}
