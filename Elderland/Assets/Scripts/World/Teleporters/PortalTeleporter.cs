f05f71df85eeef1363f2d9ced60806f0c5c695ec
liamscurry <32856600+liamscurry@users.noreply.github.com>
2021-02-11 17:23:55 -0600

    Added location, rotation and fov sync for teleporters and their target teleporters.

---
+++ Elderland/Assets/Scripts/World/Teleporters/PortalTeleporter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Construct for portal teleporters to and from the safe haven. Only renders
+// costly preview plane image when in range of trigger.
+public class PortalTeleporter : MonoBehaviour
+{
+    [SerializeField]
+    private Camera renderCamera;
+    [SerializeField]
+    private PortalTeleporter targetTeleporter;
+
+    // Fields
+    private bool drawPlane;
+
+    public void Update()
+    {
+        if (drawPlane && targetTeleporter != null)
+        {
+            Vector3 globalCameraPosition =
+                GameInfo.CameraController.transform.position;
+            Vector3 localCameraPosition = 
+                transform.worldToLocalMatrix.MultiplyPoint(globalCameraPosition);
+            Matrix4x4 targetMatrix =
+                targetTeleporter.transform.localToWorldMatrix;
+            Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 180, 0));
+            Quaternion globalCameraRotation = 
+                targetTeleporter.transform.rotation * GameInfo.CameraController.transform.rotation;
+            Vector3 targetGlobalPosition = 
+                rotationMatrix.MultiplyPoint(localCameraPosition);
+            targetGlobalPosition = 
+                targetMatrix.MultiplyPoint(targetGlobalPosition);
+            renderCamera.transform.position = targetGlobalPosition;
+            renderCamera.transform.rotation = globalCameraRotation;
+
+            renderCamera.fieldOfView = GameInfo.CameraController.Camera.fieldOfView;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == TagConstants.PlayerHitbox)
+        {
+            drawPlane = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == TagConstants.PlayerHitbox)
+        {
+            drawPlane = false;
+        }
+    }
+}
