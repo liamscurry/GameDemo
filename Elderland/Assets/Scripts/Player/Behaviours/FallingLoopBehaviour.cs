7a1de03c735e441c8d50ad20e1aed340e84d97f3
liamscurry <32856600+liamscurry@users.noreply.github.com>
2021-07-05 13:39:32 -0500

    Added high fall animations and smoothed low fall animations.

---
+++ Elderland/Assets/Scripts/Player/Behaviours/FallingLoopBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingLoopBehaviour : StateMachineBehaviour 
+{
+	private float fastestFallingSpeed;
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		fastestFallingSpeed = PlayerInfo.CharMoveSystem.GravityVelocity.y;
+	}
+
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		if (animator.GetBool(AnimationConstants.Player.Falling))
+		{
+			if (PlayerInfo.CharMoveSystem.GravityVelocity.y < fastestFallingSpeed)
+				fastestFallingSpeed = PlayerInfo.CharMoveSystem.GravityVelocity.y;
+
+			if (PlayerInfo.CharMoveSystem.Grounded)
+			{
+				if (fastestFallingSpeed < -15)
+				{
+					//Going fast, transition to landing animation
+					animator.SetInteger(AnimationConstants.Player.FallSpeed, 1);
+				}
+				else
+				{
+					animator.SetInteger(AnimationConstants.Player.FallSpeed, 0);
+				}
+
+				animator.SetBool(AnimationConstants.Player.Falling, false);
+			}	
+		}
+	}
+}
